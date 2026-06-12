5483ae03d3a566080bb0ac9437a4b3c3bd24a6ae
André König <andre.koenig@openformation.io>
2026-06-12 16:27:24 +0000

    fix(website): pin hovercard link rows

--- website/test/hovercard-css-test.sh
+++ website/test/hovercard-css-test.sh
@@ -12,3 +12,15 @@ if [[ ! $css =~ grid-column:[[:space:]]*2\; ]]; then
 	printf 'hovercard divider should render in the reserved divider column\n' >&2
 	exit 1
 fi
+
+for placement in \
+	"a:nth-child\\(1\\)[^{]*\\{[[:space:]]*grid-column:[[:space:]]*1;[[:space:]]*grid-row:[[:space:]]*1;" \
+	"a:nth-child\\(2\\)[^{]*\\{[[:space:]]*grid-column:[[:space:]]*1;[[:space:]]*grid-row:[[:space:]]*2;" \
+	"a:nth-child\\(3\\)[^{]*\\{[[:space:]]*grid-column:[[:space:]]*3;[[:space:]]*grid-row:[[:space:]]*1;" \
+	"a:nth-child\\(4\\)[^{]*\\{[[:space:]]*grid-column:[[:space:]]*3;[[:space:]]*grid-row:[[:space:]]*2;"
+do
+	if [[ ! $css =~ $placement ]]; then
+		printf 'hovercard links should pin each link to its intended grid cell\n' >&2
+		exit 1
+	fi
+done
